584982617dc564fa578d5c2d9bce04c5acea81d6
andrewmanzano <andoromanzano@gmail.com>
2026-08-21 17:07:27 +0800

    Tag

---
+++ ConsoleApp1/HelpDeskSystem/Models/Tag.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Helpdesksystem.Models;
+
+[Table("Tags")]
+public class Tag
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    public ICollection<TicketTag> TicketTags { get; set; } = new List<TicketTag>();
+}
