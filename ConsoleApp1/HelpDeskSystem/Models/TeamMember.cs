80191c2f283a89ccdf5074f8bc0811b496994606
ANTONIOMARCKEVIN <klyneskevin0818@gmail.com>
2026-08-21 16:38:01 +0800

    Add Team entity model only

---
+++ ConsoleApp1/HelpDeskSystem/Models/TeamMember.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HelpDeskSystem.Models
+{
+    [Table("TeamMembers")]
+    public class TeamMember
+    {
+        public int TeamId { get; set; }
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public string JoinedAt { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(TeamId))]
+        public Team Team { get; set; } = null!;
+
+        [ForeignKey(nameof(EmployeeId))]
+        public Employee Employee { get; set; } = null!;
+    }
+}
