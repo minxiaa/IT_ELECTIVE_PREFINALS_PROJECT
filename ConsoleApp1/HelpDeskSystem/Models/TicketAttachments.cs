b1ee50a0b539de0c7081d8329391375f2e3fd1d5
minxiaa <hanayopotato@gmail.com>
2026-08-21 16:53:28 +0800

    ticket attachments

---
+++ ConsoleApp1/HelpDeskSystem/Models/TicketAttachments.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace HelpDeskSystem;
+
+[Table("TicketAttachments")]
+public class TicketAttachment
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public int TicketId { get; set; }
+
+    [Required]
+    public string FileName { get; set; } = string.Empty;
+
+    [Required]
+    public string FilePath { get; set; } = string.Empty;
+
+    [Required]
+    public DateTime UploadedAt { get; set; }
+
+    [ForeignKey(nameof(TicketId))]
+    public Ticket? Ticket { get; set; }
+}
