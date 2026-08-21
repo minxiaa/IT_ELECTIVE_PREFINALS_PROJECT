e32a93d151da2074750e1432e9c2cf0cf8c74d3a
andrewmanzano <andoromanzano@gmail.com>
2026-08-21 16:49:00 +0800

    TIcketTag.cs

---
+++ ConsoleApp1/HelpDeskSystem/Models/TicketTag.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Helpdesksystem.Models;
+
+[Table("TicketTags")]
+public class TicketTag
+{
+    public int TicketId { get; set; }
+
+    public int TagId { get; set; }
+
+    [ForeignKey(nameof(TagId))]
+    public Tag Tag { get; set; } = null!;
+}
