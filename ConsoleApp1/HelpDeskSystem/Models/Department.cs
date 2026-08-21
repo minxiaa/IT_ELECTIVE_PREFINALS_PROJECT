9a0baa7b9502115d26e049c402e794419afb074c
andrewmanzano <andoromanzano@gmail.com>
2026-08-21 16:38:31 +0800

    Department.cs

---
+++ ConsoleApp1/HelpDeskSystem/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Helpdesksystem.Models;
+
+[Table("Departments")]
+public class Department
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public bool IsActive { get; set; } = true;
+
+    public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+}
