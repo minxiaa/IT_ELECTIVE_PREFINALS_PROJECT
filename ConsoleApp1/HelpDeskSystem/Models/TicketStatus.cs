e2964142280e516baeebf4cb81e2e94bd2e14118
ANTONIOMARCKEVIN <klyneskevin0818@gmail.com>
2026-08-21 17:28:38 +0800

    Add TicketStatus model

---
+++ ConsoleApp1/HelpDeskSystem/Models/TicketStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelpDeskSystem.Models
+{
+    public class TicketStatus
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
