4a1feb4751c9efe59ad45c668120f371b67f7c3c
minxiaa <hanayopotato@gmail.com>
2026-08-21 16:49:28 +0800

    ticketcategoriess

---
+++ ConsoleApp1/HelpDeskSystem/Models/TicketCategories.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace HelpDeskSystem;
+
+[Table("TicketCategories")]
+public class TicketCategory
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int? ParentCategoryId { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+
+ 
+    [ForeignKey(nameof(ParentCategoryId))]
+    public TicketCategory? ParentCategory { get; set; }
+
+  
+    public ICollection<TicketCategory> Children { get; set; }
+        = new List<TicketCategory>();
+
+ 
+    public ICollection<Ticket> Tickets { get; set; }
+        = new List<Ticket>();
+}
