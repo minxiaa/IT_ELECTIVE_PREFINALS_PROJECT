44f4bea7374ffc2234514c56140c3ac8ae4ee692
andrewmanzano <andoromanzano@gmail.com>
2026-08-21 16:40:52 +0800

    Employee.cs

---
+++ ConsoleApp1/HelpDeskSystem/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Helpdesksystem.Models;
+
+[Table("Employees")]
+public class Employee
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string FullName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    public string? JobTitle { get; set; }
+
+    [Required]
+    public int DepartmentId { get; set; }
+
+    public bool IsActive { get; set; } = true;
+
+    [ForeignKey(nameof(DepartmentId))]
+    public Department Department { get; set; } = null!;
+}
