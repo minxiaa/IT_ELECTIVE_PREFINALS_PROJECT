8cccddaf1c1205db579d5996452305696f4a9344
ANTONIOMARCKEVIN <klyneskevin0818@gmail.com>
2026-08-21 17:14:38 +0800

    Add CustomersController

---
+++ ConsoleApp1/HelpDeskSystem/Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HelpDeskSystem.Data;
+using HelpDeskSystem.Models;
+
+namespace HelpDeskSystem.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Customers
+        public async Task<IActionResult> Index()
+        {
+            var customers = await _context.Customers.ToListAsync();
+            return View(customers);
+        }
+    }
+}
