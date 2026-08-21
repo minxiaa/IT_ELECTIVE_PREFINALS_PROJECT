992e55a09c80c4d60d87d1f29d0f652804c113ae
ANTONIOMARCKEVIN <klyneskevin0818@gmail.com>
2026-08-21 17:13:13 +0800

    Add TeamsController

---
+++ ConsoleApp1/HelpDeskSystem/Controllers/TeamsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HelpDeskSystem.Data;
+using HelpDeskSystem.Models;
+
+namespace HelpDeskSystem.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TeamsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Teams
+        public async Task<IActionResult> Index()
+        {
+            var teams = await _context.Teams.Include(t => t.Department).ToListAsync();
+            return View(teams);
+        }
+    }
+}
