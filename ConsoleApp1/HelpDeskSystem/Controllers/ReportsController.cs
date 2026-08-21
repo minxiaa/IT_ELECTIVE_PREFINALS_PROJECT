7c15a97fdb33fc27b6509ff2367e2c425de07ed8
andrewmanzano <andoromanzano@gmail.com>
2026-08-21 20:29:44 +0800

    ReportController

---
+++ ConsoleApp1/HelpDeskSystem/Controllers/ReportsController.cs
@@ -0,0 +1,50 @@
+﻿using Helpdesksystem.Models.ViewModels;
+using HelpDeskSystem.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Helpdesksystem.Controllers;
+
+public class ReportsController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public ReportsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> EmployeeWorkload()
+    {
+        var workload = await _context.Employees
+            .Where(e => e.IsActive)
+            .Select(e => new EmployeeWorkloadViewModel
+            {
+                EmployeeName = e.FullName,
+                DepartmentName = e.Department.Name,
+                UnresolvedTicketCount = 0 
+            })
+            .ToListAsync();
+
+        return View(workload);
+    }
+
+    public async Task<IActionResult> DepartmentWorkload()
+    {
+        var workload = await _context.Departments
+            .Select(d => new DepartmentWorkloadViewModel
+            {
+                DepartmentName = d.Name,
+                EmployeeCount = d.Employees.Count,
+                UnresolvedTicketCount = 0 
+            })
+            .ToListAsync();
+
+        return View(workload);
+    }
+}
