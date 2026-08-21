cc6771e71399a0e5f7f372977e7703f8453af881
ANTONIOMARCKEVIN <klyneskevin0818@gmail.com>
2026-08-21 16:37:52 +0800

    Add Team entity model

---
+++ ConsoleApp1/HelpDeskSystem/Models/Team.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HelpDeskSystem.Models
+{
+    [Table("Teams")]
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        [ForeignKey(nameof(DepartmentId))]
+        public Department Department { get; set; } = null!;
+
+        public ICollection<TeamMember> TeamMembers { get; set; } = new List<TeamMember>();
+    }
+}
